bccc830a5258e57925bd189ff779a913ed1e0b09
Ajar Vashisth <ajar.vashisth@dexcom.com>
2019-06-05 14:56:38 -0700

    java

--- k8s/bin/setenv.sh
+++ k8s/bin/setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export KUBE_NAMESPACE=sorrento
-export KUBE_APP_NAME=valley-app
+export KUBE_NAMESPACE=ramona
+export KUBE_APP_NAME=julian
 export KUBE_CLUSTER=kubernetes-p8-integrations-eu-dev-2
 export KUBE_REGION=europe-west3
 
